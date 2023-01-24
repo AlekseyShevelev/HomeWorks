0d4eb9007a26f9d0bc074f40988a1279e0ad2a69
Aleksey Shevelev <aash1972@yandex.ru>
2023-01-24 22:23:51 +0300

    Added Task27

---
+++ HomeWork04/Task27/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+int GetNumber(string message)
+{
+    int resultNumber = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out resultNumber))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или не корректное число. Повторите ввод!\n");
+        }
+    }
+
+    return Math.Abs(resultNumber);
+}
+
+int GetSumOfDigits(int number)
+{
+    int sum = 0;
+
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+
+    return sum;
+}
+
+int number = GetNumber("Введите число:");
+Console.WriteLine(GetSumOfDigits(number));
