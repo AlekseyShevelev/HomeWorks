33653539871795641ee40ac777d4e230cfecaf5f
Aleksey Shevelev <aash1972@yandex.ru>
2023-05-23 21:53:13 +0300

    Added HomeWork 6

---
+++ Java/HomeWork06/Notebook.java
@@ -0,0 +1,79 @@
+﻿public class Notebook {
+    private String model;
+    private String vendor;
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
+    
+    public Notebook(String model, String vendor, int ram, int hdd, String os, String color) {
+        this.model = model;
+        this.vendor = vendor;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        if (model == null) {
+            throw new IllegalStateException("Название модели не может быть null");
+        }
+        this.model = model;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        if (ram <= 0) {
+            throw new IllegalStateException("Объем оперативной памяти должен быть положительным числом.");
+        }
+        this.ram = ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        if (ram <= 0) {
+            throw new IllegalStateException("Объем жесткого диска должен быть положительным числом.");
+        }
+        this.hdd = hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s (RAM-%d/HDD-%d/%s)", vendor, model, ram, hdd, os);
+    }
+}
---
+++ Java/HomeWork06/Task01.java
@@ -0,0 +1,91 @@
+﻿import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Set<Notebook> notebooks = new HashSet<>();
+
+        notebooks.add(new Notebook("HP001-8-1024-W-BL", "HP", 8, 1024, "Windows", "black"));
+        notebooks.add(new Notebook("HP002-4-512-L-GR", "HP", 4, 512, "Linux", "gray"));
+        notebooks.add(new Notebook("DL001-8-512-L-GR", "Dell", 8, 512, "Linux", "gray"));
+        notebooks.add(new Notebook("DL002-4-512-D-SL", "Dell", 4, 512, "DOS", "silver"));
+        notebooks.add(new Notebook("LN001-4-256-D-WT", "Lenovo", 4, 256, "DOS", "white"));
+        notebooks.add(new Notebook("LN002-16-2048-W-BL", "Lenovo", 16, 2048, "Windows", "black"));
+
+        getNotebooks(notebooks);
+    }
+
+    private static void getNotebooks(Set<Notebook> notebooks) {
+        Map<Integer, String> filters = getFilters();
+
+        for (Notebook notebook : notebooks) {
+            boolean isFound = true;
+
+            for (Map.Entry<Integer, String> entry : filters.entrySet()) {
+                switch (entry.getKey()) {
+                    case 1:
+                        isFound = (notebook.getVendor().equals(entry.getValue()));
+                        break;
+
+                    case 2:
+                        isFound = (notebook.getRam() >= Integer.parseInt(entry.getValue()));
+                        break;
+
+                    case 3:
+                        isFound = (notebook.getHdd() >= Integer.parseInt(entry.getValue()));
+                        break;
+
+                    case 4:
+                        isFound = (notebook.getOs().equals(entry.getValue()));
+                        break;
+
+                    case 5:
+                        isFound = (notebook.getColor().equals(entry.getValue()));
+                        break;
+                }
+
+                if (!isFound) {
+                    break;
+                }
+
+            }
+
+            if (isFound) {
+                System.out.println(notebook);
+            }
+        }
+    }
+
+    private static Map<Integer, String> getFilters() {
+        Map<Integer, String> result = new HashMap<>();
+
+        Scanner scn = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите цифру, соответствующую необходимому критерию:\n" +
+                    "1-Производитель;\n" +
+                    "2-Объем ОЗУ (не менее);\n" +
+                    "3-Объем ЖД (не менее);\n" +
+                    "4-Операционная система;\n" +
+                    "5-Цвет;\n" +
+                    "0-Начать поиск");
+            int key = Integer.parseInt(scn.nextLine());
+
+            if (key == 0) {
+                break;
+            }
+
+            System.out.println("Введите значение критерия:");
+            String value = scn.nextLine();
+
+            result.put(key, value);
+        }
+
+        scn.close();
+
+        return result;
+    }
+}
