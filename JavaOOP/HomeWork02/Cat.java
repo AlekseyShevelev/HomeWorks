420fe213f4c199b3e1c874903d669fe0e63636e3
Aleksey Shevelev <aash1972@yandex.ru>
2023-06-01 00:44:06 +0300

    Added HomeWork 2

---
+++ JavaOOP/HomeWork02/Cat.java
@@ -0,0 +1,34 @@
+﻿public class Cat {
+    private String name;
+    private int appetite;
+    private boolean full;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.full = false;
+    }
+
+    public void eat(Plate plate) {
+        if (plate.getFood() >= appetite) {
+            plate.setFood(plate.getFood() - appetite);
+            full = true;
+        }
+        else {
+            System.out.println("В тарелке мало еды!");
+            full = false;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean isFull() {
+        return full;
+    }
+}
---
+++ JavaOOP/HomeWork02/MainClass.java
@@ -0,0 +1,27 @@
+﻿public class MainClass {
+    public static void main(String[] args) {
+        Plate plate = new Plate(100);
+        Cat[] cats = {
+            new Cat("Барсик", 5),
+            new Cat("Василий", 50),
+            new Cat("Пушистик", 20),
+            new Cat("Тимофей", 30),
+            new Cat("Мурзик", 10)
+        };
+
+        feedingCats(plate, cats);
+
+        plate.addFood(200);
+
+        feedingCats(plate, cats);
+    }
+    
+    private static void feedingCats(Plate plate, Cat[] cats) {
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            System.out.printf("Кот %s %s.\n", cat.getName(), (cat.isFull()) ? "сыт" : "голоден");
+        }
+
+        System.out.println("--------------");
+    }
+}
