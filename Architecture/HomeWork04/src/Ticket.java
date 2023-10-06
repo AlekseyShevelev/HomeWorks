580c3a7ef23cb167a0c73bc345f3f34d3275961f
Aleksey Shevelev <aash1972@yandex.ru>
2023-10-06 22:06:12 +0300

    Added HomeWork04

---
+++ Architecture/HomeWork04/src/Ticket.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Ticket {
+    public int rootNumber;
+    public int place;
+    public int price;
+    public Date date;
+    public boolean isValid;
+
+    public Ticket(int rootNumber, int place, int price, Date date, boolean isValid) {
+        this.rootNumber = rootNumber;
+        this.place = place;
+        this.price = price;
+        this.date = date;
+        this.isValid = isValid;
+    }
+}
---
+++ Architecture/HomeWork04/src/TicketProvider.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class TicketProvider {
+    public List<Ticket> getTicket(int rootNumber) {
+        System.out.println("Получение списка билетов...");
+        return null;
+    }
+
+    public boolean updateTicket(Ticket ticket) {
+        System.out.println("Изменение билета...");
+        return true;
+    }
+}
